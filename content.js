5a21afb4378f9a97c386ad329f11e03b9c7353d8
@Pcoi94 <107702714+Pcoi94@users.noreply.github.com>
2025-02-08 23:12:11 +0100

    v1.1

--- content.js
+++ content.js
@@ -1,36 +1,46 @@
 function injectStyles() {
-    const styleSheet = document.createElement("style");
+  const existingStyle = document.querySelector('style[data-roblox-styler]');
+  if (existingStyle) return;
 
-    styleSheet.textContent = `
+  const styleSheet = document.createElement("style");
+
+  styleSheet.textContent = `
     .dark-theme .btn-common-play-game-lg,
-    .dark-theme .btn-growth-xs,
     .light-theme .btn-common-play-game-lg,
-    .light-theme .btn-growth-xs {
+    .rbx-private-game-server-join,
+    .friend-presence-info .btn-growth-sm.btn-full-width, 
+    .game-server-join-btn {
       background-color: #00b06f !important;
       box-shadow: 0 0 5px rgb(0, 94, 59) !important;
       transition: all 0.1s ease !important;
     }
-      
+
     .dark-theme .btn-common-play-game-lg:hover,
     .dark-theme .btn-common-play-game-lg:focus,
     .light-theme .btn-common-play-game-lg:hover,
-    .light-theme .btn-common-play-game-lg:focus {
-      background-color:rgb(0, 192, 122) !important;
+    .light-theme .btn-common-play-game-lg:focus,
+    .friend-presence-info .btn-growth-sm.btn-full-width:hover, 
+    .friend-presence-info .btn-growth-sm.btn-full-width:focus,
+    .rbx-private-game-server-join:hover,
+    .rbx-private-game-server-join:focus,
+    .game-server-join-btn:hover,
+    .game-server-join-btn:focus {
+      background-color: rgb(0, 192, 122) !important;
       box-shadow: 0 0 5px rgb(0, 110, 70) !important;
     }`;
 
-    document.head.appendChild(styleSheet);
+  document.head.appendChild(styleSheet);
 }
 
 const observer = new MutationObserver(() => {
-    if (!document.querySelector('style[data-roblox-styler]')) {
-        injectStyles();
-    }
+  if (!document.querySelector('style[data-roblox-styler]')) {
+    injectStyles();
+  }
 });
 
 observer.observe(document.body, {
-    childList: true,
-    subtree: true
+  childList: true,
+  subtree: true
 });
 
 injectStyles();
