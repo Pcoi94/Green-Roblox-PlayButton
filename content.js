8c1331568a2c168797f09f28096827c179324de0
@Pcoi94 <107702714+Pcoi94@users.noreply.github.com>
2025-02-08 23:16:34 +0100

    v1.1

--- content.js
+++ content.js
@@ -8,19 +8,21 @@ function injectStyles() {
     .dark-theme .btn-common-play-game-lg,
     .light-theme .btn-common-play-game-lg,
     .rbx-private-game-server-join,
-    .friend-presence-info .btn-growth-sm.btn-full-width, 
+    .friend-presence-info .btn-growth-sm.btn-full-width,
+  .profile-join-game .btn-growth-md, 
     .game-server-join-btn {
       background-color: #00b06f !important;
       box-shadow: 0 0 5px rgb(0, 94, 59) !important;
       transition: all 0.1s ease !important;
     }
-
     .dark-theme .btn-common-play-game-lg:hover,
     .dark-theme .btn-common-play-game-lg:focus,
     .light-theme .btn-common-play-game-lg:hover,
     .light-theme .btn-common-play-game-lg:focus,
     .friend-presence-info .btn-growth-sm.btn-full-width:hover, 
     .friend-presence-info .btn-growth-sm.btn-full-width:focus,
+    .profile-join-game .btn-growth-md:hover, 
+    .profile-join-game .btn-growth-md:focus, 
     .rbx-private-game-server-join:hover,
     .rbx-private-game-server-join:focus,
     .game-server-join-btn:hover,
